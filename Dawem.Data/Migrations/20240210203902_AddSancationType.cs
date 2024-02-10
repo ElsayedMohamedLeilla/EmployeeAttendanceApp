1700dea906c374224f494bb39cb2f7147f95a8cc
Elsayed Leilla <elsayed_mohammed70@hotmail.com>
2024-02-10 22:40:09 +0200

    update database

---
+++ Dawem.Data/Migrations/20240210203902_AddSancationType.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dawem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSancationType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                schema: "Dawem",
+                table: "Sanctions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                schema: "Dawem",
+                table: "Sanctions");
+        }
+    }
+}

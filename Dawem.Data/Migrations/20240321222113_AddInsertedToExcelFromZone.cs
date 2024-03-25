e48adb24b9a33da24ead2d07c644683c70c78046
Elsayed Leilla <elsayed_mohammed70@hotmail.com>
2024-03-25 06:03:09 +0200

    fix

--- Dawem.Data/Migrations/20240321222113_AddInsertedToExcelFromZone.cs
+++ Dawem.Data/Migrations/20240321222113_AddInsertedToExcelFromZone.cs
@@ -10,22 +10,22 @@ namespace Dawem.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<bool>(
-                name: "InsertedFromExcel",
-                schema: "Dawem",
-                table: "Zones",
-                type: "bit",
-                nullable: false,
-                defaultValue: false);
+            //migrationBuilder.AddColumn<bool>(
+            //    name: "InsertedFromExcel",
+            //    schema: "Dawem",
+            //    table: "Zones",
+            //    type: "bit",
+            //    nullable: false,
+            //    defaultValue: false);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "InsertedFromExcel",
-                schema: "Dawem",
-                table: "Zones");
+            //migrationBuilder.DropColumn(
+            //    name: "InsertedFromExcel",
+            //    schema: "Dawem",
+            //    table: "Zones");
         }
     }
 }

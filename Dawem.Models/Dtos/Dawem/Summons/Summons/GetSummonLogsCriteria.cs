7ca25da59ef3eb865e65f0559f3e3034f9e20935
Elsayed Leilla <elsayed_mohammed70@hotmail.com>
2024-04-08 06:19:26 +0200

    filter summon logs

--- Dawem.Models/Dtos/Dawem/Summons/Summons/GetSummonLogsCriteria.cs
+++ Dawem.Models/Dtos/Dawem/Summons/Summons/GetSummonLogsCriteria.cs
@@ -1,8 +1,13 @@
-﻿using Dawem.Models.Criteria;
+﻿using Dawem.Enums.Generals;
+using Dawem.Models.Criteria;
 
 namespace Dawem.Models.Dtos.Dawem.Summons.Summons
 {
     public class GetSummonLogsCriteria : BaseCriteria
     {
+        public SummonDoneStatus? SummonDoneStatus { get; set; }
+        public DateTime? SummonDate { get; set; }
+        public int? EmployeeNumber {get; set;}
+        public int? SummonCode { get; set; }
     }
 }
---
+++ Enums/Generals/SummonDoneStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Dawem.Enums.Generals
+{
+    public enum SummonDoneStatus
+    {
+        Done,
+        NotDone
+    }
+}

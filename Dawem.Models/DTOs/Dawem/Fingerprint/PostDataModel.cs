d1ce2685df25a978cca44bb6484b5790648f2ffe
Elsayed Leilla <elsayed_mohammed70@hotmail.com>
2024-05-04 01:19:16 +0300

    test

---
+++ Dawem.Models/DTOs/Dawem/Fingerprint/PostDataModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Dawem.Models.DTOs.Dawem.Generic
+{
+    public class PostDataModel
+    {
+        public string SN { get; set; }
+        public string Table { get; set; }
+        public string INFO { get; set; }
+        public string Stamp { get; set; }
+        public string Options { get; set; }
+        public string Pushver { get; set; }
+        public string Language { get; set; }
+        public string PushCommkey { get; set; }
+    }
+}

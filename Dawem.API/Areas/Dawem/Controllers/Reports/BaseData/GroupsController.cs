63a8d54865bf9f3599a1fd025513c37257a2d2dd
Elsayed Leilla <elsayed_mohammed70@hotmail.com>
2024-08-19 23:15:03 +0300

    reports

---
+++ Dawem.API/Areas/Dawem/Controllers/Reports/BaseData/GroupsController.cs
@@ -0,0 +1,41 @@
+﻿using Dawem.Contract.BusinessLogic.Dawem.Reports;
+using Dawem.Enums.Generals;
+using Dawem.Models.Response.Dawem.ReportCritrias;
+using Dawem.Translations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dawem.API.Areas.Dawem.Controllers.Reports.BaseData
+{
+    [Route(LeillaKeys.DawemApiControllerAction), ApiController, DawemAuthorize]
+    public class GroupsController : DawemControllerBase
+    {
+        private readonly IReportGeneratorBL _reportGeneratorBL;
+        public GroupsController(IReportGeneratorBL reportGeneratorBL)
+        {
+            _reportGeneratorBL = reportGeneratorBL;
+        }
+        [HttpGet]
+        public IActionResult GetGroupsReport([FromQuery] GroupsReportCritria param)
+        {
+            var response = _reportGeneratorBL.GenerateGroupsReport(param);
+            if (response != null && response.IsSuccessStatusCode)
+            {
+                var contentStream = response.Content.ReadAsStream();
+                switch (param.ExportFormat)
+                {
+                    case ExportFormat.Pdf:
+                        return File(contentStream, "application/pdf", "GroupsReport.pdf");
+                    case ExportFormat.Excel:
+                        // Return Excel file
+                        // return File(contentStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "CompaniesReport.xlsx");
+                        break;
+                    // Handle other export types as needed
+                    default:
+                        // Handle unsupported export types
+                        break;
+                }
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Dawem.API/Areas/Dawem/Controllers/Reports/BaseData/SchedulesController.cs
@@ -0,0 +1,41 @@
+﻿using Dawem.Contract.BusinessLogic.Dawem.Reports;
+using Dawem.Enums.Generals;
+using Dawem.Models.Response.Dawem.ReportCritrias;
+using Dawem.Translations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dawem.API.Areas.Dawem.Controllers.Reports.BaseData
+{
+    [Route(LeillaKeys.DawemApiControllerAction), ApiController, DawemAuthorize]
+    public class SchedulesController : DawemControllerBase
+    {
+        private readonly IReportGeneratorBL _reportGeneratorBL;
+        public SchedulesController(IReportGeneratorBL reportGeneratorBL)
+        {
+            _reportGeneratorBL = reportGeneratorBL;
+        }
+        [HttpGet]
+        public IActionResult GetSchedulesReport([FromQuery] SchedulesReportCritria param)
+        {
+            var response = _reportGeneratorBL.GenerateSchedulesReport(param);
+            if (response != null && response.IsSuccessStatusCode)
+            {
+                var contentStream = response.Content.ReadAsStream();
+                switch (param.ExportFormat)
+                {
+                    case ExportFormat.Pdf:
+                        return File(contentStream, "application/pdf", "SchedulesReport.pdf");
+                    case ExportFormat.Excel:
+                        // Return Excel file
+                        // return File(contentStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "CompaniesReport.xlsx");
+                        break;
+                    // Handle other export types as needed
+                    default:
+                        // Handle unsupported export types
+                        break;
+                }
+            }
+            return NotFound();
+        }
+    }
+}
